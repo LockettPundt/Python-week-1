32bf4415f3586a05dd6a1e1ef8877ce6d60af761
Lockett Pundt <kalifornium@hotmail.com>
2020-01-16 15:20:55 -0500

    takes a number that was provided by the user and multiplies it by itself.

---
+++ week1_day3.py
@@ -0,0 +1,8 @@
+#prompt user for a number and multiply it by itself.
+#int() converts the input, which is a string, to an integer.
+user_input = int(input("Enter a number...please. "))
+
+#assign result the value of user_input multiplied by itself.
+result = user_input * user_input
+
+print(result)
