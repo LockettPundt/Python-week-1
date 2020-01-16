38e0faa9c971604cdba10b3adfc9da78e9b40e22
Lockett Pundt <kalifornium@hotmail.com>
2020-01-16 15:27:22 -0500

    takes a word and capitalizes the first letter. it then asks for a string and uses .title() to cap every word in it.

---
+++ string_capitalize.py
@@ -0,0 +1,6 @@
+#caps only the first word in the string.
+user_input = input("Enter a lowercase word. \n")
+print(user_input[0].upper() + user_input[1:])
+#will cap ever word in the string.
+input_2 = input("Enter a string.. \n")
+print(input_2.title())
