900efc3b487ad2fbf1face91968fe6ec5c544f91
Lockett Pundt <kalifornium@hotmail.com>
2020-01-16 15:20:01 -0500

    asks user for their name and shows it in all uppercase as well as how many characters are in it.

---
+++ hello.py
@@ -0,0 +1,5 @@
+user_name = input("Hi there. What is your name? ")
+
+print ("\n\nExcellent, " + user_name + ". So nice to meet you..")
+print("\n\nOh. Here is your name in upper case: " + user_name.upper()+ ".")
+print("\n\nIt also has " + str(len(user_name)) + " letters in it.\n\n")
