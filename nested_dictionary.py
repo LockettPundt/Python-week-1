e1f3ca721398afbb50470e96f346249f9ed6d9e3
Lockett Pundt <kalifornium@hotmail.com>
2020-01-17 15:25:29 -0500

    accessing items in a nested dictionary.

---
+++ nested_dictionary.py
@@ -0,0 +1,22 @@
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+print(ramit['email'])
+print(ramit['interests'][0])
+print(ramit['friends'][0]['email'])
+print(ramit['friends'][1]['interests'][1])
