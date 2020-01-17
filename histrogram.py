ce0ec15bd63815cd94fcb144717891450f8df6fb
Lockett Pundt <kalifornium@hotmail.com>
2020-01-17 15:26:29 -0500

    mispelled histogram, but this is a program that prompts user for a word then prints out a histogram of what letters compose the word.

---
+++ histrogram.py
@@ -0,0 +1,14 @@
+#prints out a histogram from a user supplied word.
+
+user_word = input("Enter a word and it will return a histogram. \n")
+
+alphabet = {}
+for x in user_word:
+    if x in alphabet:
+        alphabet[x] += 1
+    else:
+        alphabet[x] = 1
+
+print(alphabet)
+for item in alphabet:
+    print(item + " : " + "*" * alphabet[item])
