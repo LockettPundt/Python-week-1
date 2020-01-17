a0c24ddb27cf5eff69f76d8dececcac5754016a2
Lockett Pundt <kalifornium@hotmail.com>
2020-01-17 12:11:30 -0500

    made it so it can add any number of matricies. it was only able to add 2 in it's previous form.

--- matrix_addition.py
+++ matrix_addition.py
@@ -4,15 +4,17 @@ matrix_2 = [[4, 6], [25,5]]
 
 def matrix_adder(x, y):
     if (len(x) == len(y)):
-        sum_matrix = [[],[]]
+        sum_matrix = []
         i = 0
         j = 0
         while i < len(x):
+            temp = []
             while j < len(x[i]):
-                sum_matrix[i].append(x[i][j] + y[i][j])
+                temp.append(x[i][j] + y[i][j])
                 j += 1
             i += 1
             j = 0
+            sum_matrix.append(temp)
     return sum_matrix
     
 print(matrix_adder(matrix_1, matrix_2))
